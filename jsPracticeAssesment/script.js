753358b6c368229cba9012d87f9dfbb815cbe7ec
glorifier7 <johnpaularlos28@gmail.com>
2022-11-15 19:03:59 +0800

    update

---
+++ jsPracticeAssesment/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+let userName = prompt ("Enter email or username");
+let passWord = prompt ("Enter a password");
+let details = (userName + passWord);
+
+if(userName === studentgmail.com === true){
+    userName = prompt("SuccessFully Log in");
+};
+if(passWord === discovery === true){
+    passWord = prompt("SuccessFully Log in");
+};
+console.log(details);
--- jsScriptActivity2/script.js
+++ jsScriptActivity2/script.js
@@ -1,15 +1,27 @@
-console.log("Connected");
+//Prompt a user to enter a number.
+let number = prompt ("Give me a number");
 
-let number = prompt ("Give me a numbers");
+for (number; number >= 0; number--)
+{
+    //When a number is less than to 50, we need to display the message that we need to terminate the loop
 
-function enterNum (number){
-    if (number <= 50) {
-        console.log("Currently at number 20. Terminating the loop");
+    if (number < 50){
+        console.log(`Currently at number ${number}. Terminating the loop`);
+        break; //jumps out of the loop
     }
-    else if (number %= 10) {
-        console.log("The number is divisible by 10. Skipping the number");
-    }
-    
-}
 
-enterNum(number);
+    //When a number is divisible by 10, display a message that it is divisible by 10. Therefore, they will skip the number.
+    if (number % 10 === 0)
+    {
+        console.log(`The number is divisible by 10. Skipping the number`);
+        continue; //jumps over one iteration in the loop
+    }
+
+    //If it is divisible by 5, display the number.
+
+    if (number % 5 === 0)
+    {
+        console.log(number);
+    }
+
+} 
