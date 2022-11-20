e6571e3172eb109cee01a758b950b203a0d1bef1
glorifier7 <johnpaularlos28@gmail.com>
2022-11-21 01:31:18 +0800

    update

---
+++ jsScriptActivity3/script.js
@@ -0,0 +1,17 @@
+console.log("Connected");
+
+
+let user = prompt("Enter your weight in kilogram(kg).");
+const userWeight = (user * 2.20462262);
+console.log(userWeight);
+
+let message = ("Your weight in pounds(lbs) is ");
+console.log(message+userWeight);
+alert(message+userWeight);
+
+
+
+
+
+
+ 
