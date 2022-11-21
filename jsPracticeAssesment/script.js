e55b1974a9b3ae38175283a50a6d279d85f1f0fc
glorifier7 <johnpaularlos28@gmail.com>
2022-11-22 00:05:35 +0800

    update

--- jsPracticeAssesment/script.js
+++ jsPracticeAssesment/script.js
@@ -2,56 +2,56 @@
 
 
 //Use an external JS file and ensure that it is connected to index.html
-console.log("connected");
+// console.log("Hello World");
 
-//Create multiple variables that will store the different JavaScript data types containing information relating to user details.
-let firstName = "John";
-console.log(`First Name: ${firstName}`);
+// //Create multiple variables that will store the different JavaScript data types containing information relating to user details.
+// let firstName = "John";
+// console.log(`First Name: ${firstName}`);
 
-let lastName = "Smith";
-let age = 30;
-let hobbies = ["Biking", "Mountain", "Climbing", "Swimming"];
-let workAddress = {
-    houseNumber: "32",
-    street: "Washington",
-    city: "Lincoln",
-    state: "Nebraska"
-};
+// let lastName = "Smith";
+// let age = 30;
+// let hobbies = ["Biking", "Mountain", "Climbing", "Swimming"];
+// let workAddress = {
+//     houseNumber: "32",
+//     street: "Washington",
+//     city: "Lincoln",
+//     state: "Nebraska"
+// };
 
-console.log(`Last Name: ${lastName}`);
-console.log(`Age: ${age}`);
-console.log(`Hobbies: `);
-console.log(hobbies);
-console.log(`Work Address: `);
-console.log(workAddress);
+// console.log(`Last Name: ${lastName}`);
+// console.log(`Age: ${age}`);
+// console.log(`Hobbies: `);
+// console.log(hobbies);
+// console.log(`Work Address: `);
+// console.log(workAddress);
 
-//Create a function that will accept the first name, last name and age as arguments and print those details in the console as a single string. This function will also print the hobbies and work address.
+// //Create a function that will accept the first name, last name and age as arguments and print those details in the console as a single string. This function will also print the hobbies and work address.
 
-function printUserInfo (fn, ln, Age) {
-                        //parameters
-    console.log(`My name is ${fn} ${ln}, ${Age} years of age`);
+// function printUserInfo (fn, ln, Age) {
+//                         //parameters
+//     console.log(`My name is ${fn} ${ln}, ${Age} years of age`);
 
-    console.log(`This was printed inside of a function: `);
-    console.log(hobbies);
+//     console.log(`This was printed inside of a function: `);
+//     console.log(hobbies);
 
-    console.log(`This was printed inside of a function: `);
-    console.log(workAddress);
-}
+//     console.log(`This was printed inside of a function: `);
+//     console.log(workAddress);
+// }
 
-//invoke the function and provide arguments
-printUserInfo("Jelly", "Lumactud", "20");
+// //invoke the function and provide arguments
+// printUserInfo("Jelly", "Lumactud", "20");
 
-//Create another function that will return a value and store it in a variable.
-function returnValue() {
-    return true;
-    //The return statement stops the execution of a function and returns a value.
-}
+// //Create another function that will return a value and store it in a variable.
+// function returnValue() {
+//     return true;
+//     //The return statement stops the execution of a function and returns a value.
+// }
 
 // console.log(returnValue);
 
-let isMarried = returnValue();
+// let isMarried = returnValue();
 
-console.log(`The value of isMarrried is: ${isMarried}`);
+// console.log(`The value of isMarrried is: ${isMarried}`);
 
 //Part 2 - Nested IFs
 /*
@@ -113,24 +113,68 @@ console.log(`The value of isMarrried is: ${isMarried}`);
     Password: discovery
 */
 
-let i = 3;
-while (i != 0)
-{
-    let userName = "student@mail.com"
-    let uname = prompt ("Enter a username or email");
+// let i = 3;
+// while (i != 0)
+// {
+//     let userName = "student@mail.com"
+//     let uname = prompt ("Enter a username or email");
 
-    let userPassword = "discovery";
-    let pass = prompt ("Enter a password");
+//     let userPassword = "discovery";
+//     let pass = prompt ("Enter a password");
 
-    if (userName == uname && userPassword == pass)
-    {
-        i = 4 - i;
-        alert ("Successfully Login");
-        break;
+//     if (userName == uname && userPassword == pass)
+//     {
+//         i = 4 - i;
+//         alert ("Successfully Login");
+//         break;
+//     }
+
+//     else {
+//         i = i-1;
+//         alert ("Login Failed Now Only"+i+"Login Attempts Available");
+//     }
+// }
+// STEP 1
+console.log("Hello World");
+let student = ["John", "Jane", "Joe"];
+let message = (" is added to the student's list");
+console.log(student[0] + message);
+console.log(student[1] + message);
+console.log(student[2] + message);
+console.log("There are total number of 3 students in the student's list");
+
+// STEP 2
+console.log(student[1]);
+console.log(student[2]);
+console.log(student[0]);
+// STEP 3
+let foundJoe = student.find(
+    function() {
+       return (student !== "Joe")
     }
+);
 
-    else {
-        i = i-1;
-        alert ("Login Failed Now Only"+i+"Login Attempts Available");
-    }
+if (foundJoe === undefined) {
+    console.log(`Joe is not found on the list`);
 }
+
+else {
+    console.log(`Joe is an Enrollee`);
+};
+// STEP 4
+let section = [" - Section A"];
+let s1 = student[1]+section;
+let s2 = student[2]+section;
+let s3 = student[0]+section;
+let sInfo = [s1, s2, s3];
+console.log(sInfo);
+// STEP 5
+function removeJoe() {
+    console.log(student[2].slice);
+    if (removeJoe === true) {
+        console.log("Joe was removed from student list");
+    }
+    else(console.log(student));
+}
+
+removeJoe();
