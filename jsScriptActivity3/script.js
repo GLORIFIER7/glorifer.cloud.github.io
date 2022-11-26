0a1728bac8d446e406192ac7d7dbf9e142c463ee
glorifier7 <johnpaularlos28@gmail.com>
2022-11-26 17:06:09 +0800

    update

--- jsScriptActivity3/script.js
+++ jsScriptActivity3/script.js
@@ -1,17 +1,33 @@
 console.log("Connected");
 
 
-let user = prompt("Enter your weight in kilogram(kg).");
-const userWeight = (user * 2.20462262);
+// let user = prompt("Enter your weight in kilogram(kg).");
+// const userWeight = (user * 2.20462262);
+// console.log(userWeight);
+
+// let message = ("Your weight in pounds(lbs) is ");
+// console.log(message+userWeight);
+// alert(message+userWeight);
+
+
+class userWeight{
+
+    constructor (user, message)
+    {
+        this.user = user;
+        this.message = message;
+        
+    }
+}
 console.log(userWeight);
+let user = prompt("Enter your weight in kilogram(kg).");
+console.log(user);
+const userWeightKgLbs = (user * 2.20462262);
+console.log(userWeightKgLbs);
 
 let message = ("Your weight in pounds(lbs) is ");
-console.log(message+userWeight);
-alert(message+userWeight);
-
-
-
-
+console.log(message+userWeightKgLbs);
+alert(message+userWeightKgLbs);
 
 
  
