4029982dd622bc7d1f592a1193fb410e6caf94e4
glorifier7 <johnpaularlos28@gmail.com>
2022-11-26 09:33:01 +0800

    update

---
+++ s21/algorithms.js
@@ -0,0 +1,95 @@
+console.log("connected");
+
+//recursion
+/*
+    Syntax:
+    function recurse(){
+        //function code
+        recurse();
+        //function code
+    }
+    recurse();
+*/
+
+// let countDown = function func(fromNumber) {
+//     console.log(fromNumber);
+
+//     let nextNum = fromNumber -1;
+
+//     if(nextNum > 0){
+//         func(nextNum);
+//     }
+//     else {
+//         alert ("Happy New Year");
+//     }
+// }
+
+// let newYearCountDown = countDown;
+// countDown = null;
+// newYearCountDown(5);
+
+//factorial using a recursion
+
+function factorial(x){
+    //if number is 0
+    if (x === 0){
+        return 1
+    }
+    else {
+        return x * factorial(x-1);
+    }
+}
+
+const num = 3;
+
+if (num > 0){
+    let result = factorial(num);
+    console.log(`The factorial of ${num} is ${result}`);
+}
+
+//Linear Search - applicable for unsorted arrays. It counts the number of index.
+
+/*
+    PSEUDOCODE
+    1. Create a variable list to store in an array. Then create a variable where we can search the value.
+    2. Create a function that will accept two parameters/arugements for an array and a value.
+    3. Inside the function create a loop through the entire array and check if the current item is equals to a value.
+    4. if the value is found it will return the value otherwise it will return false
+*/
+
+//Step 1. Create a variable list to store in an array. Then create a variable where we can search the value.
+let list = [9,26,6,14,3,27,21,45,111,587];
+let value = 3;
+
+//Step 2. Create a function that will accept two parameters/arguments for an array and a value.
+
+let linearSearch = (list,value) => {
+    //Step 3. Inside the function create a loop through the entire array and check if the current item is equals to a value.
+    for(let i=0; i < list.length; i++){
+        if(list[i] === value)
+        {
+            return i;
+        }
+    }
+    return -1; //false stament
+}
+
+//invoke the function in the console log
+console.log(linearSearch(list,value)); //element 3 is in the index 4
+
+//linear search for grocery items
+let items = ["yakult","cheese","soju","chocolate","chuckie","dishwashing liquid", "colgate", "toyo", "sardinas"];
+let isFound = false;
+
+items.forEach(function (itemValue, index){
+    if(itemValue === "toyo")
+    {
+        console.log("Item Found: "+itemValue+ " at index "+index);
+        isFound = true;
+        return;
+    }
+});
+
+if (!isFound){
+    console.log("Item not found");
+}
--- s21/script.js
+++ s21/script.js
@@ -82,6 +82,13 @@ queue.clear();
 console.log(queue.items); //it returns an empty array due to the methods that we declared in the class.
 
 //Data Structure - Linked List
+class Node {
+    //constructor
+    constructor(element){
+        this.element = element;
+        this.next = null;
+    }
+}
 class LinkedList {
     constructor(){
         this.head = null;
@@ -89,17 +96,17 @@ class LinkedList {
     }
     //add an element at the end of the list
 
-    add(element){
+    addElement(elements){
 
         //create a new node
-        let node = new Node(element);
+        let nodes = new Node(elements);
 
         //to store the current node
         let current;
 
         //if the list is empty add the element and make it as a head
-        if(this.head = null){
-            this.head = node;
+        if(this.head == null){
+            this.head = nodes;
         }
         else {
             current = this.head;
@@ -110,9 +117,186 @@ class LinkedList {
             }
 
             //add node
-            current.next = node;
+            current.next = nodes;
         }
         this.size++;
     }
-    
+
+    //insert an element at the position index of the list
+    insertAt(element,index) {
+
+        if(index < 0 || index > this.size) {
+            return console.log("Please enter a valid index");
+        }
+        else {
+            //create a new node
+            let node = new Node(element);
+
+            let curr,prev;
+
+            curr = this.head;
+
+            //add the elements to the first index
+            if(index == 0){
+                node.next = this.head;
+                this.head = node;
+            }
+            else {
+                curr = this.head;
+                let it = 0; //means iterate
+
+                //iterate over the first to find the position to insert.
+                while(it < index){
+                    it++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+
+                //adding an element 
+                node.next = curr;
+                prev.next = node;
+            }
+
+        this.size++
+
+        }
+        
+    }
+
+    //remove an element from the specified location.
+
+    removeFrom(index){
+        if(index < 0 || index >= this.size){
+            return console.log("Please enter a valid index");
+        } else {
+            let curr, prev, it = 0;
+
+            curr = this.head;
+            prev = curr;
+
+            //deleting first element
+            if (index === 0)
+            {
+                this.head = curr.next;
+            }
+            else 
+            {
+                //iterate over the list to the position to remove an element
+                while (it < index){
+                    it++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+
+                //remove the element
+                prev.next = curr.next;
+
+            }
+            this.size--;
+
+            //return the renove element
+            return curr.element;
+        }
+    }
+
+    //remove a given element from the list
+    removeElement (element){
+        let current = this.head;
+        let prev = null;
+
+        //loop ovr the list
+        while (current != null) {
+            //compare element with the current element
+            //if the element is found then we need to remove and returned as true
+
+            if(current.element === element){
+                if (prev == null){
+                    this.head = current.next;
+                }
+                else {
+                    prev.next = current.next;
+                }
+            }
+            this.size--;
+            return current.element;
+
+        }
+        return -1;
+    }
+
+    //find an index in the element
+    indexOf(e){
+        let count = 0;
+        let current = this.head;
+
+        //loop over the list
+
+        while (current != null){
+            //compare each element to the list within the given elements
+
+            if (current.e === e){
+                return count;
+            }
+            count++;
+            current = current.next;
+        }
+
+        //if the index is not found
+        return -1;
+    }
+
+    //checking if the list is empty
+    isEmpty(){
+        return this.size == 0;
+    }
+
+    //give the size of the list
+    size_of_list(){
+        console.log(this.size);
+    }
+
+    //printing the list of items
+    printList(){
+        let curr = this.head;
+        let str = "";
+        while (curr){
+            str += curr.element + "";
+            curr = curr.next;
+        }
+        console.log(str);
+    }
 }
+
+//creating an object for the linkedList class
+let linkedList = new LinkedList();
+
+//check if the function isEmpty on an empty list.
+console.log(linkedList.isEmpty());
+//it returns true
+
+//add elements to the list
+linkedList.addElement(10);
+
+linkedList.printList();
+
+console.log(linkedList.size_of_list());
+
+//add more elements to the list
+linkedList.addElement(20);
+linkedList.addElement(30);
+linkedList.addElement(40);
+linkedList.addElement(50);
+
+//return the numbers provided
+linkedList.printList();
+
+console.log("is element removed ?" +linkedList.removeElement(10));
+
+linkedList.printList(); //removes 10
+
+console.log(linkedList.indexOf(20));
+
+linkedList.insertAt(60, 4);
+linkedList.printList();
+
+console.log("Is my list empty? ",linkedList.isEmpty()); //false - boolean
